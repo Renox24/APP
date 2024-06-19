71da52be54cb62d1f4983c8f6bc2ce6d096b4b60
Renox24 <167002428+Renox24@users.noreply.github.com>
2024-06-19 11:44:37 +0200

    Create mtn.js

---
+++ javascript/mtn.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.maintenance-container');
+    container.style.opacity = 1;
+    container.style.transform = 'translateY(0)';
+});
